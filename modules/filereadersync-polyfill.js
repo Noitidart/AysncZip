fdbde62e9e965d4057bbc1355c515035cd8b1256
Noitidart <noitidart@gmail.com>
2015-11-18 13:41:30 -0800

    inital.rev9
    Cleaned up polyfill per
    https://github.com/gildas-lormeau/zip.js/issues/135#issuecomment-157702327

--- modules/filereadersync-polyfill.js
+++ modules/filereadersync-polyfill.js
@@ -1,80 +1,40 @@
 // Creates FileReader polyfill from FileReaderSync
 // This is filereader-polyfill.js (warning: not tested).
-var GLOBE = this;
 (function() {
-    if (GLOBE.FileReader || !GLOBE.FileReaderSync) {
-        return;
-    }
-    function createExpandoEvent(obj, name) {
+	if (self.FileReader || !self.FileReaderSync) {
 		return;
-        var listenerCached = null;
-		console.log('j0');
-        Object.defineProperty(this, 'on' + name, {
-            set: function(listener) {
-                if (typeof (listener && listener.handleEvent || listener) != 'function')
-                    listener = null;
-				// this.removeEventListener(name, listenerCached);
-                listenerCached = listener;
-                // this.addEventListener(name, listener);
-            },
-            get: function() {
-                return listenerCached;
-            }
-        });
-		console.log('jEND');
-    }
-    function defineFileReaderMethod(methodName) {
-        FileReader.prototype[methodName] = function() {
+	}
+
+	function defineFileReaderMethod(methodName) {
+		FileReader.prototype[methodName] = function() {
 			console.log('called methodName:', methodName);
-            var args = [].slice.call(arguments);
-			console.log('args:', args);
-            var fr = this;
-            setTimeout(function() {
-				console.log('triggered setTimeout for methodName:', methodName);
-                try {
-					console.log('will now apply');
-					console.log('fr.thisFileReaderSync.readAsArrayBuffer:', fr.thisFileReaderSync[methodName]);
+			var args = [].slice.call(arguments);
+			var fr = this;
+			setTimeout(function() {
+				try {
 					fr.result = fr.thisFileReaderSync[methodName].apply(fr.thisFileReaderSync, args);
-                    // fr.dispatchEvent(new CustomEvent('load'));
-                    // fr.dispatchEvent(new CustomEvent('loadend'));
-					if (fr.onload) {
-						fr.onload({
-							target: {
-								result: fr.result
-							}
-						});
-					}
-					if (fr.onloadend) {
-						fr.onloadend({
-							target: {
-								result: fr.result
-							}
-						});
-					}
-					console.log('ok dispatched custom events');
-                } catch (e) {
-					console.error('got e:', e);
-                    fr.error = e;
-                    // fr.dispatchEvent(new CustomEvent('error'));
+				} catch (e) {
+					fr.error = e;
 					if (fr.onerror) {
-						fr.onerror({
-							error: fr.error
-						});
+						fr.onerror({error:fr});
 					}
-                }
-            }); // Async
-        };
-    }
-    GLOBE.FileReader = function() {
-		console.log('creating expando 1');
-        createExpandoEvent(this, 'load');
-		console.log('creating expando 2');
-        createExpandoEvent(this, 'error');
-		console.log('expandos created');
+				}
+				if (fr.onload) {
+					fr.onload({target:fr});
+				}
+				if (fr.onloadend) {
+					fr.onloadend({target:fr});
+				}
+			}); // Async
+		};
+	}
+  
+	self.FileReader = function() {
 		this.thisFileReaderSync = new FileReaderSync();
-    };
-    FileReader.prototype = Object.create(FileReaderSync.prototype);
-    defineFileReaderMethod('readAsText');
-    defineFileReaderMethod('readAsArrayBuffer');
-    defineFileReaderMethod('readAsDataURL');
+	};
+  
+	FileReader.prototype = Object.create(FileReaderSync.prototype);
+	defineFileReaderMethod('readAsText');
+	defineFileReaderMethod('readAsArrayBuffer');
+	defineFileReaderMethod('readAsDataURL');
 })();
